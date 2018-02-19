23bfa46c1d10ad1db6848709c0dade934a6c9f1b
megatkhs <tarodino0526@gmail.com>
2018-02-19 21:15:08 +0900

    Day 8 FontAwesome5が無理だったから4にした 思ってたのができなかったから明日やりたい void()とかいう池沼な記述を削除

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,20 @@
 
 const targets = document.getElementsByTagName('a')
 
-for(let target of targets)
+for( let target of targets )
   target.addEventListener('click', (e) => { e.preventDefault() })
+
+
+// const blend_target = document.getElementsByClassName('blend')
+// for ( let target of blend_target ) {
+//   const t = target.children[1]
+//   const span = [document.createElement('span'), document.createElement('span')]
+//   const className = ['blend_top', 'blend_bottom']
+//   const text = t.innerText
+//   console.log(span)
+//   for( let i = 0; i < span.length; i ++ ) {
+//     span[i].setAttribute('class', className[i])
+//     span[i].innerText = text
+//     t.appendChild(span[i])
+//   }
+// }
